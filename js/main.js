f713b25c5b3ad29bcda8cf2d12c1f62629bbc0a3
Lucas Leite <lbl.lucasleite@gmail.com>
2019-04-01 20:33:10 -0300

    feat: setting up the Service Worker

--- js/main.js
+++ js/main.js
@@ -10,8 +10,20 @@ var markers = []
 document.addEventListener('DOMContentLoaded', (event) => {
   fetchNeighborhoods();
   fetchCuisines();
+  registerServiceWorker();
 });
 
+registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  }
+}
 /**
  * Fetch all neighborhoods and set their HTML.
  */
